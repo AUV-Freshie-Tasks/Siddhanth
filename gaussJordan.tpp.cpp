89638ac94729e0428727e3fe6a85a391619adcdc
TSiddhanth <siddhanth.tadepalli@outlook.com>
2025-12-09 11:24:53 +0530

    Add files via upload

---
+++ gaussJordan.tpp.cpp
@@ -0,0 +1,63 @@
+#include"matrix.hpp"
+#include<cmath>
+#include<algorithm>
+using namespace std;
+
+Matrix Matrix::gaussJordan(){
+	int r = row;
+	Matrix mtr(m1);
+
+	vector<vector<float>> idv(r, vector<float>(r, 0)); //making identity matrix for the augment thing
+	for(int i = 0; i<r; i++){
+		for(int j=0; j<r; j++){
+			if(i==j) idv[i][j] = 1.0;
+		}
+	}
+	Matrix idm(idv);
+
+
+
+
+
+	for(int i = 0; i < r; i++){
+		float mx = -10000000;
+		int idx;
+		for(int j = i; j < r; j++){
+			float temp = std::max((float)mx, (float)mtr[j][i]);
+			mx = temp;
+			if(mtr[j][i] == mx)
+				idx = j;
+			
+		}	
+		if(mx != 0){
+			mtr.swapRow(i+1, idx+1);
+			idm.swapRow(i+1, idx+1);
+			//cout<<"Current Mtr::rowSwap"<<endl;mtr.print();
+		}
+		else
+			break;
+		for(int div=0; div<r; div++){
+			mtr[i][div] /= mx; idm[i][div] /= mx;
+			//cout<<"Current Mtr::division"<<endl;mtr.print();
+			}
+		for(int sub = 0; sub<r; sub++){
+			if(sub != i){
+			float x = mtr[sub][i]/mtr[i][i];
+				for(int k = 0; k < r; k++){
+					mtr[sub][k] -= x*mtr[i][k];
+					idm[sub][k] -= x*idm[i][k];
+					//cout<<"Current Mtr::subtraction"<<endl;mtr.print();
+				}
+			}
+		}
+		
+	}
+	return Matrix(idm);
+	
+
+
+
+	/*int main(){
+i		return 0;
+	}*/
+}
---
+++ matrix.hpp
@@ -0,0 +1,27 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include<vector>
+#include<iostream>
+#include<list>
+using namespace std;
+
+//template<typename T>
+class Matrix{
+public:
+	vector<vector<float>> m1; int row; int col;
+	Matrix(vector<vector<float>>& a);
+	vector<float>& operator[](int i);
+	void swapRow(int r1, int r2);
+	Matrix swapCol(int c1, int c2);
+	void Transpose();
+	Matrix operator+(Matrix b);
+	Matrix operator*(Matrix b);
+	Matrix gaussJordan();
+	void print();
+	struct LUCacheNode;
+	list<LUCacheNode> luCache;
+	int cap;
+	Matrix decompLU(Matrix a, Matrix b);
+};
+#endif
